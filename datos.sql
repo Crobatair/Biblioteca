021a43b58a25dd59d7abb2b6fc111c33af958cf8
Crobatair <jose5m45@gmail.com>
2016-06-04 12:51:35 -0600

    Cambio a SQL de base de datos y nuevo script
    Agregado Datos,sql que contiene los datos de prueba, y modificado el script de la base de datos

---
+++ datos.sql
@@ -0,0 +1,253 @@
+	----------------------------
+	----------------------------	OK				TABLA PAIS
+	----------------------------	OK
+	----------------------------
+	INSERT INTO pais (nombre) VALUES('Alaska');
+	INSERT INTO pais (nombre) VALUES('Albania');
+	INSERT INTO pais (nombre) VALUES('Alemania');
+	INSERT INTO pais (nombre) VALUES('Andorra');
+	INSERT INTO pais (nombre) VALUES('Angola');
+	INSERT INTO pais (nombre) VALUES('Arabia Saudí');
+	INSERT INTO pais (nombre) VALUES('Argelia');
+	INSERT INTO pais (nombre) VALUES('Argentina');
+	INSERT INTO pais (nombre) VALUES('Armenia');
+	INSERT INTO pais (nombre) VALUES('Australia');
+	INSERT INTO pais (nombre) VALUES('Austria');
+	INSERT INTO pais (nombre) VALUES('Bahreim');
+	INSERT INTO pais (nombre) VALUES('Bangladesh');
+	INSERT INTO pais (nombre) VALUES('Bélgica');
+	INSERT INTO pais (nombre) VALUES('Bolivia');
+	INSERT INTO pais (nombre) VALUES('Bosnia');
+	INSERT INTO pais (nombre) VALUES('Brasil');
+	INSERT INTO pais (nombre) VALUES('Bulgaria');
+	INSERT INTO pais (nombre) VALUES('Cabo Verde');
+	INSERT INTO pais (nombre) VALUES('Camboya');
+	INSERT INTO pais (nombre) VALUES('Camerún');
+	INSERT INTO pais (nombre) VALUES('Canadá');
+	INSERT INTO pais (nombre) VALUES('Centroafricana, Rep.');
+	INSERT INTO pais (nombre) VALUES('Checa, Rep.');
+	INSERT INTO pais (nombre) VALUES('Chile');
+	INSERT INTO pais (nombre) VALUES('China');
+	INSERT INTO pais (nombre) VALUES('Chipre');
+	INSERT INTO pais (nombre) VALUES('Colombia');
+	INSERT INTO pais (nombre) VALUES('Congo, Rep. del');
+	INSERT INTO pais (nombre) VALUES('Congo, Rep. Democ.');
+	INSERT INTO pais (nombre) VALUES('Corea, Rep.');
+	INSERT INTO pais (nombre) VALUES('Corea, Rep. Democ.');
+	INSERT INTO pais (nombre) VALUES('Costa de Marfil');
+	INSERT INTO pais (nombre) VALUES('Costa Rica');
+	INSERT INTO pais (nombre) VALUES('Croacia');
+	INSERT INTO pais (nombre) VALUES('Cuba');
+	INSERT INTO pais (nombre) VALUES('Dinamarca');
+	INSERT INTO pais (nombre) VALUES('Dominicana, Rep.');
+	INSERT INTO pais (nombre) VALUES('Ecuador');
+	INSERT INTO pais (nombre) VALUES('Egipto');
+	INSERT INTO pais (nombre) VALUES('El Salvador');
+	INSERT INTO pais (nombre) VALUES('Emiratos Árabes Unidos');
+	INSERT INTO pais (nombre) VALUES('Eslovaca, Rep.');
+	INSERT INTO pais (nombre) VALUES('Eslovenia');
+	INSERT INTO pais (nombre) VALUES('España');
+	INSERT INTO pais (nombre) VALUES('Estados Unidos');
+	INSERT INTO pais (nombre) VALUES('Estonia');
+	INSERT INTO pais (nombre) VALUES('Etiopía');
+	INSERT INTO pais (nombre) VALUES('Filipinas');
+	INSERT INTO pais (nombre) VALUES('Finlandia');
+	INSERT INTO pais (nombre) VALUES('Francia');
+	INSERT INTO pais (nombre) VALUES('Gibraltar');
+	INSERT INTO pais (nombre) VALUES('Grecia');
+	INSERT INTO pais (nombre) VALUES('Groenlandia');
+	INSERT INTO pais (nombre) VALUES('Guatemala');
+	INSERT INTO pais (nombre) VALUES('Guinea Ecuatorial');
+	INSERT INTO pais (nombre) VALUES('Haití');
+	INSERT INTO pais (nombre) VALUES('Hawai');
+	INSERT INTO pais (nombre) VALUES('Honduras');
+	INSERT INTO pais (nombre) VALUES('Hong Kong');
+	INSERT INTO pais (nombre) VALUES('Hungría');
+	INSERT INTO pais (nombre) VALUES('India');
+	INSERT INTO pais (nombre) VALUES('Indonesia');
+	INSERT INTO pais (nombre) VALUES('Irak');
+	INSERT INTO pais (nombre) VALUES('Irán');
+	INSERT INTO pais (nombre) VALUES('Irlanda');
+	INSERT INTO pais (nombre) VALUES('Islandia');
+	INSERT INTO pais (nombre) VALUES('Israel');
+	INSERT INTO pais (nombre) VALUES('Italia');
+	INSERT INTO pais (nombre) VALUES('Jamaica');
+	INSERT INTO pais (nombre) VALUES('Japón');
+	INSERT INTO pais (nombre) VALUES('Jordania');
+	INSERT INTO pais (nombre) VALUES('Kenia');
+	INSERT INTO pais (nombre) VALUES('Kuwait');
+	INSERT INTO pais (nombre) VALUES('Laos');
+	INSERT INTO pais (nombre) VALUES('Letonia');
+	INSERT INTO pais (nombre) VALUES('Líbano');
+	INSERT INTO pais (nombre) VALUES('Liberia');
+	INSERT INTO pais (nombre) VALUES('Libia');
+	INSERT INTO pais (nombre) VALUES('Liechtenstein');
+	INSERT INTO pais (nombre) VALUES('Lituania');
+	INSERT INTO pais (nombre) VALUES('Luxemburgo');
+	INSERT INTO pais (nombre) VALUES('Madagascar');
+	INSERT INTO pais (nombre) VALUES('Malasia');
+	INSERT INTO pais (nombre) VALUES('Malta');
+	INSERT INTO pais (nombre) VALUES('Marruecos');
+	INSERT INTO pais (nombre) VALUES('Martinica');
+	INSERT INTO pais (nombre) VALUES('Mauritania');
+	INSERT INTO pais (nombre) VALUES('México');
+	INSERT INTO pais (nombre) VALUES('Moldavia');
+	INSERT INTO pais (nombre) VALUES('Mónaco');
+	INSERT INTO pais (nombre) VALUES('Mongolia');
+	INSERT INTO pais (nombre) VALUES('Mozambique');
+	INSERT INTO pais (nombre) VALUES('Namibia');
+	INSERT INTO pais (nombre) VALUES('Nepal');
+	INSERT INTO pais (nombre) VALUES('Nicaragua');
+	INSERT INTO pais (nombre) VALUES('Nigeria');
+	INSERT INTO pais (nombre) VALUES('Noruega');
+	INSERT INTO pais (nombre) VALUES('Nueva Zelanda');
+	INSERT INTO pais (nombre) VALUES('Países Bajos');
+	INSERT INTO pais (nombre) VALUES('Pakistán');
+	INSERT INTO pais (nombre) VALUES('Panamá');
+	INSERT INTO pais (nombre) VALUES('Paraguay');
+	INSERT INTO pais (nombre) VALUES('Perú');
+	INSERT INTO pais (nombre) VALUES('Polonia');
+	INSERT INTO pais (nombre) VALUES('Portugal');
+	INSERT INTO pais (nombre) VALUES('Puerto Rico');
+	INSERT INTO pais (nombre) VALUES('Qatar');
+	INSERT INTO pais (nombre) VALUES('Reino Unido');
+	INSERT INTO pais (nombre) VALUES('Rumania');
+	INSERT INTO pais (nombre) VALUES('Rusia');
+	INSERT INTO pais (nombre) VALUES('San Marino');
+	INSERT INTO pais (nombre) VALUES('Senegal');
+	INSERT INTO pais (nombre) VALUES('Singapur');
+	INSERT INTO pais (nombre) VALUES('Siria');
+	INSERT INTO pais (nombre) VALUES('Somalia');
+	INSERT INTO pais (nombre) VALUES('Sri-Lanka');
+	INSERT INTO pais (nombre) VALUES('Sudáfrica');
+	INSERT INTO pais (nombre) VALUES('Sudán');
+	INSERT INTO pais (nombre) VALUES('Suecia');
+	INSERT INTO pais (nombre) VALUES('Suiza');
+	INSERT INTO pais (nombre) VALUES('Tailandia');
+	INSERT INTO pais (nombre) VALUES('Taiwan');
+	INSERT INTO pais (nombre) VALUES('Tanzania');
+	INSERT INTO pais (nombre) VALUES('Túnez');
+	INSERT INTO pais (nombre) VALUES('Turquía');
+	INSERT INTO pais (nombre) VALUES('Ucrania');
+	INSERT INTO pais (nombre) VALUES('Uganda');
+	INSERT INTO pais (nombre) VALUES('Uruguay');
+	INSERT INTO pais (nombre) VALUES('Vaticano');
+	INSERT INTO pais (nombre) VALUES('Venezuela');
+	INSERT INTO pais (nombre) VALUES('Vietnam');
+	INSERT INTO pais (nombre) VALUES('Yemen');
+	INSERT INTO pais (nombre) VALUES('Yugoslavia');
+	INSERT INTO pais (nombre) VALUES('Zambia');
+	INSERT INTO pais (nombre) VALUES('Zimbawe');
+	
+	----------------------------
+	----------------------------	OK				TABLA CATEGORIA
+	----------------------------	OK
+	----------------------------
+	
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (000,'Generalidades','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (100,'Filosofía y Psicología','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (200,'Religión, Teología','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (300,'Ciencias Sociales','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (400,'Lenguas','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (500,'Ciencias Básicas','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (600,'Tecnología y Ciencias Aplicadas','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (700,'Artes y recreación','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (800,'Literatura','');
+	INSERT INTO categoria (id_categoria,categoria,descripcion) VALUES (900,'Historia y Geografía','');
+
+	----------------------------
+	----------------------------	OK				TABLA EDITORIAL
+	----------------------------	OK
+	----------------------------
+	
+	INSERT INTO editorial (editorial,descripcion) VALUES ('Editorial A','Descripciond e la editorial a');
+	INSERT INTO editorial (editorial,descripcion) VALUES ('Editorial B','Descripciond e la editorial b');
+	INSERT INTO editorial (editorial,descripcion) VALUES ('Editorial C','Descripciond e la editorial c');
+	INSERT INTO editorial (editorial,descripcion) VALUES ('Editorial D','Descripciond e la editorial d');
+	INSERT INTO editorial (editorial,descripcion) VALUES ('Editorial E','Descripciond e la editorial e');
+	INSERT INTO editorial (editorial,descripcion) VALUES ('Editorial F','Descripciond e la editorial f');
+	
+	----------------------------
+	----------------------------	OK				TABLA ESTADO EJEMPLAR
+	----------------------------	OK
+	----------------------------	
+	
+	INSERT INTO estado_ejemplar (estado,descripcion) VALUES ('Estado A','Descripcion de estado a');
+	INSERT INTO estado_ejemplar (estado,descripcion) VALUES ('Estado B','Descripcion de estado b');
+	INSERT INTO estado_ejemplar (estado,descripcion) VALUES ('Estado C','Descripcion de estado c');
+	INSERT INTO estado_ejemplar (estado,descripcion) VALUES ('Estado D','Descripcion de estado d');
+	INSERT INTO estado_ejemplar (estado,descripcion) VALUES ('Estado E','Descripcion de estado e');
+	INSERT INTO estado_ejemplar (estado,descripcion) VALUES ('Estado F','Descripcion de estado f');
+	
+	----------------------------
+	----------------------------	REVISAR			TABLA TIPO USUARIO
+	----------------------------	REVISAR
+	----------------------------
+	
+	INSERT INTO tipo_usuario (nombre,descripcion) VALUES('Estudiante Interno','Estudiante Interno de El Centro escolar y puede solicitar libros');
+	INSERT INTO tipo_usuario (nombre,descripcion) VALUES('Profesor Interno','Puede prestar libros dentro de la biblioteca para llevar');
+
+	INSERT INTO tipo_usuario (nombre,descripcion) VALUES('Estudiante Visitante','Visitante al centro educativo y solo puede prestar el libro internamente');
+	INSERT INTO tipo_usuario (nombre,descripcion) VALUES('Universitario Visitante','Universitario Visitante al centro educativo y solo puede prestar el libro internamente');	
+	INSERT INTO tipo_usuario (nombre,descripcion) VALUES('Profesor Visitante','Visitante al centro educativo y solo puede prestar el libro internamente');
+	INSERT INTO tipo_usuario (nombre,descripcion) VALUES('Particular Visitante','Visitante al centro educativo y solo puede prestar el libro internamente');
+	
+	----------------------------
+	----------------------------	REVISAR			TABLA USUARIO DE SISTEMA
+	----------------------------	REVISAR
+	----------------------------
+	
+	
+	INSERT INTO usuario_sistema (usuario_sistema,password,nombres,apellidos,direccion,numero_contacto,email,acceso_total) VALUES ('admin','admin','Administrador','de Sistema','Direccion','0000-0000','E@mail.com',TRUE);
+	
+	
+	----------------------------
+	----------------------------	REVISAR			TABLA DE USUARIO
+	----------------------------	REVISAR
+	----------------------------	
+	
+	
+	INSERT INTO usuario (usuario,id_tipo_usuario,nombre,apellidos,direccion,numero_contacto,email,dui,nie,fecha_nacimiento) VALUES ('fafa',3,'Fatima','Escalante','Calla paloma','Error404','fafa@fafi.com','Not Found','0','5/10/1996');
+	
+	----------------------------
+	----------------------------	REVISAR
+	----------------------------	REVISAR			TABLA DE LIBRO
+	----------------------------	
+	
+	INSERT INTO libro (isbn,titulo,edicion,autor,id_pais,id_editorial,id_categoria,precio) VALUES ('9788484284888','ORGULLO Y PREJUICIO','Jane Austen','1ra',5,3,000,50.00);
+	
+	
+	----------------------------
+	----------------------------	REVISAR			TABLA DE EJEMPLAR
+	----------------------------	REVISAR
+	----------------------------
+	
+	INSERT INTO ejemplar (isbn,numero_ejemplar,cod_barra,id_estado_ejemplar) VALUES ('9788484284888',1,'Cod barras del 1',2);
+	
+	INSERT INTO ejemplar (isbn,numero_ejemplar,cod_barra,id_estado_ejemplar) VALUES ('9788484284888',2,'Cod barras del 2',1);
+	
+	INSERT INTO ejemplar (isbn,numero_ejemplar,cod_barra,id_estado_ejemplar) VALUES ('9788484284888',3,'Cod barras del 3',3);
+		
+	INSERT INTO ejemplar (isbn,numero_ejemplar,cod_barra,id_estado_ejemplar) VALUES ('9788484284888',4,'Cod barras del 4',1);
+	
+	INSERT INTO ejemplar (isbn,numero_ejemplar,cod_barra,id_estado_ejemplar) VALUES ('9788484284888',5,'Cod barras del 5',1);
+	
+	INSERT INTO ejemplar (isbn,numero_ejemplar,cod_barra,id_estado_ejemplar) VALUES ('9788484284888',6,'Cod barras del 6',5);
+	
+
+	----------------------------	
+	----------------------------	REVISAR
+	----------------------------	REVISAR			TABLA DE PRESTAMO
+	----------------------------	
+	
+	INSERT INTO prestamo (usuario_sistema,usuario,observaciones) VALUES ('admin','fafa','Prestamo');
+	----------------------------
+	----------------------------	REVISAR			TABLA DETALLE PRESTAMO
+	----------------------------	REVISAR
+	----------------------------	
+	
+	INSERT INTO detalle_prestamo (isbn, id_ejemplar, id_prestamo, usuario_sistema,timestamp_devuelto,observacion) VALUES ('9788484284888',1,1,'admin',null,'lalalalla');
+	INSERT INTO detalle_prestamo (isbn, id_ejemplar, id_prestamo, usuario_sistema,timestamp_devuelto,observacion) VALUES ('9788484284888',2,1,'admin',null,'Tralalala');
+	INSERT INTO detalle_prestamo (isbn, id_ejemplar, id_prestamo, usuario_sistema,timestamp_devuelto,observacion) VALUES ('9788484284888',5,1,'admin',null,'trololol');
+	
+	
